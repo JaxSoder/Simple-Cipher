4443391779991d4f2ff8c0dbb6a121c96cc469b7
JaxSoder <41527516+JaxSoder@users.noreply.github.com>
2019-03-03 14:27:31 -0600

    test

--- HelpForm.Designer.cs
+++ HelpForm.Designer.cs
@@ -39,7 +39,8 @@
             this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.label1.Size = new System.Drawing.Size(344, 85);
             this.label1.TabIndex = 2;
-            this.label1.Text = " ";
+            this.label1.Text = "-- Jaxs Cipher currently only works with numbers. When using this tool there shou" +
+    "ld be no spaces. That can mess up the Algorithm. ";
             // 
             // label2
             // 
